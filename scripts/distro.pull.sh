66baec59488e0c66308fa995f9200606da425029
Frederick Henderson <frederickjh@henderson-meier.org>
2015-02-25 22:35:04 +0100

    Cleaning up for new version branch.

--- scripts/distro.pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GITURL="https://github.com/wet-boew/wet-boew-drupal.git"
-BRANCH="7.x-1.x"
-PREFIX="projects/wetkit"
-echo "Pulling in latest updates on branch $BRANCH from remote $GITURL."
-
-# Change to git root directory.
-cd "$(git rev-parse --show-toplevel)"
-git subtree pull --squash --prefix=$PREFIX $GITURL $BRANCH
