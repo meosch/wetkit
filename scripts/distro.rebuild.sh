1f371b60ef693556aa8ade0f340b0a12ebfbd1ea
Frederick Henderson <frederickjh@henderson-meier.org>
2015-09-14 08:03:32 +0200

    Made webroot folder user setable.

--- scripts/distro.rebuild.sh
+++ scripts/distro.rebuild.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+# What is our webroot folder called?  docroot? httpdocs? public_html?
+webroot="public_html"
+
 # Pull down latest copy of Wetkit.
 ./distro.pull.sh
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT
 
-echo "Removing docroot"
-rm -rf docroot
+echo "Removing webroot"
+rm -rf ${webroot}
 
 echo "Building Wetkit profile"
-drush make -y scripts/build-wetkit-profile.make docroot --no-gitinfofile
+drush make -y scripts/build-wetkit-profile.make ${webroot} --no-gitinfofile
 
 # Remove .gitignore files that are undesired.
-rm docroot/profiles/wetkit/.gitignore
-rm docroot/profiles/wetkit/libraries/Leaflet/.gitignore
+rm ${webroot}/profiles/wetkit/.gitignore
+rm ${webroot}/profiles/wetkit/libraries/Leaflet/.gitignore
 
-echo "Symlinking sites directory to docroot/sites"
-cd docroot
+echo "Symlinking sites directory to ${webroot}/sites"
+cd ${webroot}
 rm -rf sites
 ln -s ../sites
-echo "Symlinking .htaccess docroot/.htaccess"
+echo "Symlinking .htaccess ${webroot}/.htaccess"
 rm .htaccess
 ln -s ../.htaccess
-echo "Symlinking robots.txt to docroot/robots.txt"
+echo "Symlinking robots.txt to ${webroot}/robots.txt"
 rm robots.txt
 ln -s ../robots.txt
